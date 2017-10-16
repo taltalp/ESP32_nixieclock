93a4bab31dd30247fa21bdfea50c1f8e2cfc2e3a
taltalp <taltalpmh@gmail.com>
2017-10-17 01:08:06 +0900

    add nixie functions

--- Arduino/esp32_nixieclock.ino
+++ Arduino/esp32_nixieclock.ino
@@ -10,6 +10,91 @@ const char* password = "your-password";
 
 const int JST = 3600*(-9);
 
+const int NIXIE_A = 1;
+const int NIXIE_B = 2;
+const int NIXIE_C = 3;
+const int NIXIE_D = 4;
+
+const int NIXIE_COM_0 = 5;
+const int NIXIE_COM_1 = 6;
+const int NIXIE_COM_2 = 7;
+const int NIXIE_COM_3 = 8;
+const int NIXIE_COM_4 = 9;
+const int NIXIE_COM_5 = 10;
+
+int[] nixie_data = [0, 0, 0, 0, 0, 0];
+int[] nixie_en = [0, 0, 0, 0, 0, 0];
+
+// Init Nixie
+void initNixie(){
+    pinMode(NIXIE_A, OUTPUT);
+    pinMode(NIXIE_B, OUTPUT);
+    pinMode(NIXIE_C, OUTPUT);
+    pinMode(NIXIE_D, OUTPUT);
+
+    pinMode(NIXIE_COM_0, OUTPUT);
+    pinMode(NIXIE_COM_1, OUTPUT);
+    pinMode(NIXIE_COM_2, OUTPUT);
+    pinMode(NIXIE_COM_3, OUTPUT);
+    pinMode(NIXIE_COM_4, OUTPUT);
+    pinMode(NIXIE_COM_5, OUTPUT);
+
+    digitalWrite(NIXIE_A, LOW);
+    digitalWrite(NIXIE_B, LOW);
+    digitalWrite(NIXIE_C, LOW);
+    digitalWrite(NIXIE_D, LOW);
+
+    digitalWrite(NIXIE_COM_0, HIGH);
+    digitalWrite(NIXIE_COM_1, HIGH);
+    digitalWrite(NIXIE_COM_2, HIGH);
+    digitalWrite(NIXIE_COM_3, HIGH);
+    digitalWrite(NIXIE_COM_4, HIGH);
+    digitalWrite(NIXIE_COM_5, HIGH);  
+}
+
+// Set Nixie Value
+int setNixieValue(int num, int val){
+    if (num >= 0 && num <= 5 && val >= 0 && val <= 9) {
+        nixie_data[num] = val;
+    } else {
+        return -1;
+    }
+    return 0;
+}
+
+// Get Nixie Value
+int getNixieValue(int num){
+    if (num >= 0 && num <= 5){
+        return nixie_data[num];
+    } else {
+        return -1;
+    }
+}
+
+// Set Nixie Enable
+int setNixieEnable(int num, int val){
+    if (num >= 0 && num <= 5 && val >= 0 && val <= 1) {
+        nixie_en[num] = val;
+    } else {
+        return -1;
+    }
+    return 0;
+}
+
+// Get Nixie Enable
+int getNixieEnable(int num){
+    if (num >= 0 && num <= 5) {
+        return nixie_en[num];
+    } else {
+        return -1;
+    }
+}
+
+// Slot Nixie
+int slotNixie(){
+    return 0;
+}
+
 void setup()
 {
     Serial.begin(115200);
